085dc5e25585216f7cf4c76ddbfd325383e04711
Bae SangHyuk <44903787+D3vle0@users.noreply.github.com>
2019-01-03 12:53:10 +0900

    Delete ex2-6.c

--- ex2-6.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-	//printf 서식문자 실습
-	printf("%d년 %d월 %d일\n", 2019, 1, 2);
-	printf("%d:    \n %3d    \n", 9, 9);
-	//%f %d 실습
-	printf("정수 = %d\n실수 = %f\n", 10, 10.557);
-	printf("실수 = %.2f\n", 10.557);
-}
