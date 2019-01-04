80fc17b22832e8204a3c0b0a54f0040c168c1d16
Bae SangHyuk <44903787+D3vle0@users.noreply.github.com>
2019-01-04 15:51:11 +0900

    Update ex3-12(30120 배상혁).c

--- day3/ex3-12(30120 배상혁).c
+++ day3/ex3-12(30120 배상혁).c
@@ -1,8 +1,8 @@
-//30120 ¹è»óÇõ
+//30120 ë°°ìƒí˜
 #include <stdio.h>
 #pragma warning (disable:4996)
 int main() {
 	int a, b, c;
 	scanf("%d %d %d", &a, &b, &c);
-	((a < b + c && b < a + c && c < a + b) == 1) ? printf("yes\n") : printf("no\n");
+	((a <= 10000 && b <= 10000 && c <= 10000) == 1) ? ((a < b + c && b < a + c && c < a + b) == 1) ? printf("yes\n") : printf("no\n") : printf("\n");
 }
